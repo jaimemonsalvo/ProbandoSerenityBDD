5eee24730f270096440ac450a927afcf465066d0
jaime.monsalvo <ingenieromonsalvo@hotmail.com>
2022-10-14 13:50:50 -0500

    organizando proyecto serenity

---
+++ src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
@@ -0,0 +1,34 @@
+package starter.duckduckgo;
+
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+public class WhenSearchingByKeyword {
+
+    @Managed(driver = "chrome", options = "headless")
+    WebDriver driver;
+
+
+    NavigateActions navigate;
+    SearchActions search;
+    SearchResultSidebar searchResultSidebar;
+
+
+    @Test
+    void theKeywordShouldAppearInTheResultsSidebar() {
+
+        navigate.toTheDuckDuckGoSearchPage();
+        search.byKeyword("Cucumber");
+        assertThat(searchResultSidebar.heading()).isEqualTo("Cucumber");
+    }
+
+
+}
